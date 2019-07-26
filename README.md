78e54019b861ea6f0d8e687bdeb1495b05856315
codeweb-stack <peehu719@gmail.com>
2019-07-26 20:00:50 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cielo
+
+This program will tell you all the recipes you can make with the ingredients you have. It will accept up to 5 ingredients.
+
+### Database Structure
+
+| Recipe Name | Ingredients
+|-------------|------------|
+| Apple fudge | Apple, Milk, Sugar, Baking powder, Baking soda, Salt, Flour, Brown Sugar |
+| Banana pancake | Banana, Flour, Milk, Sugar, Salt, Whipped cream, Baking powder, Eno |
+| Cake | Flour, Sugar, Milk, Whipped cream, baking powder, ENO, Biscuit crumps |
+| Coffee | Sugar, milk, water, Coffee |
---
+++ cielo.py
@@ -0,0 +1,28 @@
+###############################################################################
+# This is my first program in python, Its name is Cielo.
+#
+# Name of program: Cielo
+# Author: Peehu Sehrawat
+# Date: 13/07/2019
+###############################################################################
+
+# Step 1: Accept upto 5 ingredients.
+
+ingredients = ['milk', 'flour', 'whipped cream']
+
+# Step 2: Database to search from.
+
+food = {
+    'apple fudge': set(['apple', 'milk', 'sugar', 'baking powder', 'baking soda', 'salt', 'flour', 'brown sugar']),
+    'banana pancake': set(['banana', 'flour', 'milk', 'sugar', 'salt', 'whipped cream', 'baking powder', 'eno']),
+    'cake':	set(['flour', 'sugar', 'milk', 'whipped cream', 'baking powder', 'eno', 'biscuit crumps']),
+    'coffee sugar': set(['milk', 'water', 'coffee'])
+}
+
+# Step 3: Search accepted ingredients in dictionary, move them to temp dictionary
+
+
+
+# Step 4: Remove accepted ingredients from the temp dictionary
+
+# Step 5: Print the result
